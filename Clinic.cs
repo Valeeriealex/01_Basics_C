8a5ee6b5fed2fd78d814e12cf3f0db6579f18187
Valerie <114693445+Valeeriealex@users.noreply.github.com>
2024-10-17 20:45:01 +0300

    Update Clinic.cs

--- Clinic.cs
+++ Clinic.cs
@@ -4,26 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//namespace Tasks_IJunior._01_Basics
-//{
-//    internal class Clinic
-//    {
-//        //Вы заходите в поликлинику и видите огромную очередь, вам нужно рассчитать время ожидания в очереди.  
+namespace Tasks_IJunior._01_Basics
+{
+   internal class Clinic
+   {
+       //Вы заходите в поликлинику и видите огромную очередь, вам нужно рассчитать время ожидания в очереди.  
 
-//        static void Main(string[] args)
-//        {
-//            int numberPeopleInLine;
-//            int timeForOnePatient;
-//            int waitingTime;
-//            int minutesInHour;
-//            int hoursOfWaiting;
-//            int minutesOfWaiting;
+       static void Main(string[] args)
+       {
+           int numberPeopleInLine;
+           int timeForOnePatient;
+           int waitingTime;
+           int minutesInHour;
+           int hoursOfWaiting;
+           int minutesOfWaiting;
 
-//            timeForOnePatient = 10;
-//            minutesInHour = 60;
+           timeForOnePatient = 10;
+           minutesInHour = 60;
 
-//            Console.WriteLine($"Вы пришли в поликлинику. Время приема одного человека - {timeForOnePatient} минут. Сколько людей перед Вами?");
-//            numberPeopleInLine = Convert.ToInt32(Console.ReadLine());
+           Console.WriteLine($"Вы пришли в поликлинику. Время приема одного человека - {timeForOnePatient} минут. Сколько людей перед Вами?");
+           numberPeopleInLine = Convert.ToInt32(Console.ReadLine());
 //            waitingTime = numberPeopleInLine * timeForOnePatient;
 //            hoursOfWaiting = waitingTime / minutesInHour;
 //            minutesOfWaiting = waitingTime % minutesInHour;
