999de348943ad9511e251c16a143371811115bd1
Valerie <114693445+Valeeriealex@users.noreply.github.com>
2024-10-17 20:55:28 +0300

    Update Variables.cs
    Переменные

--- Variables.cs
+++ Variables.cs
@@ -4,24 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//namespace Tasks_IJunior._01_Basics
-//{
-//    internal class Variables
-//    {
-//        //Переменные
+namespace Tasks_IJunior._01_Basics
+{
+   internal class Variables
+   {
 
-//        static void Main(string[] args)
-//        {
-//            int userAge = 20;
-//            uint startingAgeToBuy = 18;
-//            float amountUfMoney = 100000.90f;
-//            bool canSell = userAge >= startingAgeToBuy;
-//            string userName = "Valeriya";
-//            char letterOneInUserName = 'V';
-//            byte maxMemory = 255;
-//            sbyte fullMemory = 127;
-//            double resultOfSubtractingVariables = maxMemory - fullMemory;
-//            long maxValue = long.MaxValue;
-//        }
-//    }
-//}
+       static void Main(string[] args)
+       {
+           int userAge = 20;
+           uint startingAgeToBuy = 18;
+           float amountUfMoney = 100000.90f;
+           bool canSell = userAge >= startingAgeToBuy;
+           string userName = "Valeriya";
+           char letterOneInUserName = 'V';
+           byte maxMemory = 255;
+           sbyte fullMemory = 127;
+           double resultOfSubtractingVariables = maxMemory - fullMemory;
+           long maxValue = long.MaxValue;
+       }
+   }
+}
